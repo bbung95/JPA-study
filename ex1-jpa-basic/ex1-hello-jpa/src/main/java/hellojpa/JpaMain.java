1fdb64bf5798eef8009e4581df882e0ce3e9e4a0
bbung95 <tjd123123@naver.com>
2022-03-10 23:13:53 +0900

    JPA 기본 매핑

--- ex1-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,5 @@
 package hellojpa;
 
-import java.util.Iterator;
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -22,31 +19,6 @@ public class JpaMain {
 
 		try {
 			
-//			등록
-//			Member member = new Member();
-//			
-//			member.setId(2L);
-//			member.setName("HelloB");
-//			
-//			em.persist(member);
-			
-//			조회
-//			Member findMember = em.find(Member.class, 1L);
-			
-			List<Member> memberList = em.createQuery("select m from Member as m" , Member.class)
-		            		.setFirstResult(0)  // MySQL : LIMIT   , ORACLE : ROWNUM (시작 번호)
-				            .setMaxResults(5) // (가져오는 갯수)
-				            .getResultList();
-			
-			for (Member member : memberList) {
-				System.out.println("memeber-name : "+member.getName());
-			}
-
-// 			업데이트
-//			findMember.setName("HelloJPA");
-			
-//			삭제
-//			em.remove(findMember);
 			
 			tx.commit(); // 로직 정상 수행
 		} catch(Exception e) {
--- ex1-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,33 +1,21 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Member {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	private String name;
+	
+	@Column(name = "name")
+	private String username;
 	
 	public Member() { // JPA는 기본적으로 리플랙션을 사용하며 동적으로 쿼리를 생성하기에 기본 생성자가 꼭 필요하다.
-	}
-	
-	public Member(long id , String name)	 {
-		this.id = id;
-		this.name = name;
-	}
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
 	}
 }
