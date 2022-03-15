7cac5305f48ff970db21a96d6556211c877e3256
bbung95 <tjd123123@naver.com>
2022-03-16 02:06:34 +0900

    JPA - CasCade , 고어 객체 실전 예제

--- ex1-jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ ex1-jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -18,6 +18,9 @@ public class Delivery extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status;
 
+    @OneToOne(mappedBy = "delivery" , fetch = FetchType.LAZY)
+    private Order order;
+
     public Long getId() {
         return id;
     }
